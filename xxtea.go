a76601386c43a23a9c90f01f0c32a77b8f99f1cb
Jeff Buchbinder <jeff@ourexchange.net>
2012-09-18 17:31:12 -0400

    Fix algo.

--- xxtea.go
+++ xxtea.go
@@ -19,6 +19,24 @@ typedef unsigned int xxtea_long;
 #define XXTEA_MX (( (z >> 5) ^ (y << 2)) + ((y >> 3) ^ (z << 4))) ^ ((sum ^ y) + (k[(p & 3) ^ e] ^ z))
 #define XXTEA_DELTA 0x9e3779b9
 
+unsigned char *char_to_unsigned_array(char *orig) {
+  char *p = orig;
+  while (p != '\0') {
+    *p = (unsigned char) *p;
+    p++;
+  }
+  return (unsigned char *) orig;
+}
+
+char *unsigned_to_char_array(unsigned char *orig) {
+  unsigned char *p = orig;
+  while (p != '\0') {
+    *p = (char) *p;
+    p++;
+  }
+  return (char *) orig;
+}
+
 void xxtea_long_encrypt(xxtea_long *v, xxtea_long len, xxtea_long *k) {
     xxtea_long n = len - 1;
     xxtea_long z = v[n], y = v[0], p, q = 6 + 52 / (n + 1), sum = 0, e;
@@ -55,7 +73,7 @@ void xxtea_long_decrypt(xxtea_long *v, xxtea_long len, xxtea_long *k) {
     }
 }
 
-static xxtea_long *xxtea_to_long_array(const char *data, xxtea_long len, int include_length, xxtea_long *ret_len) {
+static xxtea_long *xxtea_to_long_array(const unsigned char *data, xxtea_long len, int include_length, xxtea_long *ret_len) {
     xxtea_long i, n, *result;
         n = len >> 2;
     n = (((len & 3) == 0) ? n : n + 1);
@@ -74,27 +92,27 @@ static xxtea_long *xxtea_to_long_array(const char *data, xxtea_long len, int inc
     return result;
 }
 
-static char *xxtea_to_byte_array(xxtea_long *data, xxtea_long len, int include_length, xxtea_long *ret_len) {
+static unsigned char *xxtea_to_byte_array(xxtea_long *data, xxtea_long len, int include_length, xxtea_long *ret_len) {
     xxtea_long i, n, m;
-    char *result;
+    unsigned char *result;
     n = len << 2;
     if (include_length) {
         m = data[len - 1];
         if ((m < n - 7) || (m > n - 4)) return NULL;
         n = m;
     }
-    result = (char *)malloc(n + 1);
+    result = (unsigned char *)malloc(n + 1);
         for (i = 0; i < n; i++) {
-        result[i] = (char)((data[i >> 2] >> ((i & 3) << 3)) & 0xff);
+        result[i] = (unsigned char)((data[i >> 2] >> ((i & 3) << 3)) & 0xff);
     }
         result[n] = '\0';
         *ret_len = n;
         return result;
 }
 
-char *xxtea_encrypt(const char *data, xxtea_long len, char *key, xxtea_long *ret_len)
+unsigned char *xxtea_encrypt(const unsigned char *data, xxtea_long len, unsigned char *key, xxtea_long *ret_len)
 {
-    char *result;
+    unsigned char *result;
     xxtea_long *v, *k, v_len, k_len;
     v = xxtea_to_long_array(data, len, 1, &v_len);
     k = xxtea_to_long_array(key, 16, 0, &k_len);
@@ -105,9 +123,9 @@ char *xxtea_encrypt(const char *data, xxtea_long len, char *key, xxtea_long *ret
     return result;
 }
 
-char *xxtea_decrypt(const char *data, xxtea_long len, char *key, xxtea_long *ret_len)
+unsigned char *xxtea_decrypt(const unsigned char *data, xxtea_long len, unsigned char *key, xxtea_long *ret_len)
 {
-    char *result;
+    unsigned char *result;
     xxtea_long *v, *k, v_len, k_len;
     v = xxtea_to_long_array(data, len, 0, &v_len);
     k = xxtea_to_long_array(key, 16, 0, &k_len);
@@ -125,23 +143,23 @@ import (
 )
 
 func Encrypt(text, key string) (out string, err error) {
-	ctext := C.CString(text)
+	ctext := C.char_to_unsigned_array(C.CString(text))
 	defer C.free(unsafe.Pointer(ctext))
-	ckey := C.CString(key)
+	ckey := C.char_to_unsigned_array(C.CString(key))
 	defer C.free(unsafe.Pointer(ckey))
 	var cretlen C.xxtea_long
 
-	out = C.GoString(C.xxtea_encrypt(ctext, C.xxtea_long(len(text)), ckey, &cretlen))
+	out = C.GoString(C.unsigned_to_char_array(C.xxtea_encrypt(ctext, C.xxtea_long(len(text)), ckey, &cretlen)))
 	return
 }
 
 func Decrypt(text, key string) (out string, err error) {
-	ctext := C.CString(text)
+	ctext := C.char_to_unsigned_array(C.CString(text))
 	defer C.free(unsafe.Pointer(ctext))
-	ckey := C.CString(key)
+	ckey := C.char_to_unsigned_array(C.CString(key))
 	defer C.free(unsafe.Pointer(ckey))
 	var cretlen C.xxtea_long
 
-	out = C.GoString(C.xxtea_decrypt(ctext, C.xxtea_long(len(text)), ckey, &cretlen))
+	out = C.GoString(C.unsigned_to_char_array(C.xxtea_decrypt(ctext, C.xxtea_long(len(text)), ckey, &cretlen)))
 	return
 }
