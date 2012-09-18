c3adc4d3f11199cf53cf46b9003ba0985fc4c3be
Jeff Buchbinder <jeff@ourexchange.net>
2012-09-18 18:05:52 -0400

    Reformat C code.

--- xxtea.go
+++ xxtea.go
@@ -1,5 +1,7 @@
 // GOXXTEA
 // https://github.com/jbuchbinder/goxxtea
+//
+// vim: tabstop=4:softtabstop=4:shiftwidth=4:noexpandtab
 
 package xxtea
 
@@ -23,122 +25,122 @@ typedef unsigned int xxtea_long;
 // Go strings and unsigned char *.
 
 unsigned char *char_to_unsigned_array(char *orig) {
-  char *p = orig;
-  while (p != '\0') {
-    *p = (unsigned char) *p;
-    p++;
-  }
-  return (unsigned char *) orig;
+	char *p = orig;
+	while (p != '\0') {
+		*p = (unsigned char) *p;
+		p++;
+	}
+	return (unsigned char *) orig;
 }
 
 char *unsigned_to_char_array(unsigned char *orig) {
-  unsigned char *p = orig;
-  while (p != '\0') {
-    *p = (char) *p;
-    p++;
-  }
-  return (char *) orig;
+	unsigned char *p = orig;
+	while (p != '\0') {
+		*p = (char) *p;
+		p++;
+	}
+	return (char *) orig;
 }
 
 // XXTEA algorithm functions
 
 void xxtea_long_encrypt(xxtea_long *v, xxtea_long len, xxtea_long *k) {
-    xxtea_long n = len - 1;
-    xxtea_long z = v[n], y = v[0], p, q = 6 + 52 / (n + 1), sum = 0, e;
-    if (n < 1) {
-        return;
-    }
-    while (0 < q--) {
-        sum += XXTEA_DELTA;
-        e = sum >> 2 & 3;
-        for (p = 0; p < n; p++) {
-            y = v[p + 1];
-            z = v[p] += XXTEA_MX;
-        }
-        y = v[0];
-        z = v[n] += XXTEA_MX;
-    }
+	xxtea_long n = len - 1;
+	xxtea_long z = v[n], y = v[0], p, q = 6 + 52 / (n + 1), sum = 0, e;
+	if (n < 1) {
+		return;
+	}
+	while (0 < q--) {
+		sum += XXTEA_DELTA;
+		e = sum >> 2 & 3;
+		for (p = 0; p < n; p++) {
+			y = v[p + 1];
+			z = v[p] += XXTEA_MX;
+		}
+		y = v[0];
+		z = v[n] += XXTEA_MX;
+	}
 }
 
 void xxtea_long_decrypt(xxtea_long *v, xxtea_long len, xxtea_long *k) {
-    xxtea_long n = len - 1;
-    xxtea_long z = v[n], y = v[0], p, q = 6 + 52 / (n + 1), sum = q * XXTEA_DELTA, e;
-    if (n < 1) {
-        return;
-    }
-    while (sum != 0) {
-        e = sum >> 2 & 3;
-        for (p = n; p > 0; p--) {
-            z = v[p - 1];
-            y = v[p] -= XXTEA_MX;
-        }
-        z = v[n];
-        y = v[0] -= XXTEA_MX;
-        sum -= XXTEA_DELTA;
-    }
+	xxtea_long n = len - 1;
+	xxtea_long z = v[n], y = v[0], p, q = 6 + 52 / (n + 1), sum = q * XXTEA_DELTA, e;
+	if (n < 1) {
+		return;
+	}
+	while (sum != 0) {
+		e = sum >> 2 & 3;
+		for (p = n; p > 0; p--) {
+			z = v[p - 1];
+			y = v[p] -= XXTEA_MX;
+		}
+		z = v[n];
+		y = v[0] -= XXTEA_MX;
+		sum -= XXTEA_DELTA;
+	}
 }
 
 static xxtea_long *xxtea_to_long_array(const unsigned char *data, xxtea_long len, int include_length, xxtea_long *ret_len) {
-    xxtea_long i, n, *result;
-        n = len >> 2;
-    n = (((len & 3) == 0) ? n : n + 1);
-    if (include_length) {
-        result = (xxtea_long *)malloc((n + 1) << 2);
-        result[n] = len;
-                *ret_len = n + 1;
-        } else {
-        result = (xxtea_long *)malloc(n << 2);
-                *ret_len = n;
-    }
-        memset(result, 0, n << 2);
-        for (i = 0; i < len; i++) {
-        result[i >> 2] |= (xxtea_long)data[i] << ((i & 3) << 3);
-    }
-    return result;
+	xxtea_long i, n, *result;
+	n = len >> 2;
+	n = (((len & 3) == 0) ? n : n + 1);
+	if (include_length) {
+		result = (xxtea_long *)malloc((n + 1) << 2);
+		result[n] = len;
+		*ret_len = n + 1;
+	} else {
+		result = (xxtea_long *)malloc(n << 2);
+		*ret_len = n;
+	}
+	memset(result, 0, n << 2);
+	for (i = 0; i < len; i++) {
+		result[i >> 2] |= (xxtea_long)data[i] << ((i & 3) << 3);
+	}
+	return result;
 }
 
 static unsigned char *xxtea_to_byte_array(xxtea_long *data, xxtea_long len, int include_length, xxtea_long *ret_len) {
-    xxtea_long i, n, m;
-    unsigned char *result;
-    n = len << 2;
-    if (include_length) {
-        m = data[len - 1];
-        if ((m < n - 7) || (m > n - 4)) return NULL;
-        n = m;
-    }
-    result = (unsigned char *)malloc(n + 1);
-        for (i = 0; i < n; i++) {
-        result[i] = (unsigned char)((data[i >> 2] >> ((i & 3) << 3)) & 0xff);
-    }
-        result[n] = '\0';
-        *ret_len = n;
-        return result;
+	xxtea_long i, n, m;
+	unsigned char *result;
+	n = len << 2;
+	if (include_length) {
+		m = data[len - 1];
+		if ((m < n - 7) || (m > n - 4)) return NULL;
+		n = m;
+	}
+	result = (unsigned char *)malloc(n + 1);
+	for (i = 0; i < n; i++) {
+		result[i] = (unsigned char)((data[i >> 2] >> ((i & 3) << 3)) & 0xff);
+	}
+	result[n] = '\0';
+	*ret_len = n;
+	return result;
 }
 
 unsigned char *xxtea_encrypt(const unsigned char *data, xxtea_long len, unsigned char *key, xxtea_long *ret_len)
 {
-    unsigned char *result;
-    xxtea_long *v, *k, v_len, k_len;
-    v = xxtea_to_long_array(data, len, 1, &v_len);
-    k = xxtea_to_long_array(key, 16, 0, &k_len);
-    xxtea_long_encrypt(v, v_len, k);
-    result = xxtea_to_byte_array(v, v_len, 0, ret_len);
-    free(v);
-    free(k);
-    return result;
+	unsigned char *result;
+	xxtea_long *v, *k, v_len, k_len;
+	v = xxtea_to_long_array(data, len, 1, &v_len);
+	k = xxtea_to_long_array(key, 16, 0, &k_len);
+	xxtea_long_encrypt(v, v_len, k);
+	result = xxtea_to_byte_array(v, v_len, 0, ret_len);
+	free(v);
+	free(k);
+	return result;
 }
 
 unsigned char *xxtea_decrypt(const unsigned char *data, xxtea_long len, unsigned char *key, xxtea_long *ret_len)
 {
-    unsigned char *result;
-    xxtea_long *v, *k, v_len, k_len;
-    v = xxtea_to_long_array(data, len, 0, &v_len);
-    k = xxtea_to_long_array(key, 16, 0, &k_len);
-    xxtea_long_decrypt(v, v_len, k);
-    result = xxtea_to_byte_array(v, v_len, 1, ret_len);
-    free(v);
-    free(k);
-    return result;
+	unsigned char *result;
+	xxtea_long *v, *k, v_len, k_len;
+	v = xxtea_to_long_array(data, len, 0, &v_len);
+	k = xxtea_to_long_array(key, 16, 0, &k_len);
+	xxtea_long_decrypt(v, v_len, k);
+	result = xxtea_to_byte_array(v, v_len, 1, ret_len);
+	free(v);
+	free(k);
+	return result;
 }
 */
 import "C"
