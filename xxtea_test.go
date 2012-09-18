c32d272b4350a4a6a64fff8fba74b480147592a2
Jeff Buchbinder <jeff@ourexchange.net>
2012-09-18 17:11:46 -0400

    Fix test.

--- xxtea_test.go
+++ xxtea_test.go
@@ -11,9 +11,9 @@ func Test_XXTEA_Roundtrip(t *testing.T) {
 	key := "xxteaTEST"
 	orig := "The quick brown fox jumped over the lazy dog."
 
-	intermediate, _ := Encode(orig, key)
+	intermediate, _ := Encrypt(orig, key)
 
-	decoded, _ := Decode(intermediate, key)
+	decoded, _ := Decrypt(intermediate, key)
 
 	if decoded != orig {
 		t.Error("Round trip encode failed, intermediate = " + intermediate)
